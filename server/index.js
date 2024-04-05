fa360247806f3214d9aa188ba929e2592cde4d61
sidd-bash <77910988+sidd-bash@users.noreply.github.com>
2024-04-05 15:52:29 +0530

    Server Added

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const {Pool} = require('pg')
+const app = express();
+const dotenv = require('dotenv')
+dotenv.config()
+
+app.use(cors({origin:'*'}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(8000,()=>{
+    console.log('Server is listening at port 8000')
+})
+const config = {connectionString: process.env.POSTGRES_URL}
+const pool = new Pool(config)
+
+app.get('/gameBanners',async (req,res)=>{
+    const result = await pool.query('SELECT image, id, name from games')
+    res.send(result.rows)
+})
